7764e81969859d6be8151bfe01b994217324d530
Trayana Nedelcheva <trayana.nedelcheva@sap.com>
2026-06-21 00:14:56 +0300

    chore: remove docs, testing and database migration scripts from remote tracking

--- database/check-admin.sql
+++
@@ -1,5 +0,0 @@
--- Check admin user
-SELECT id, email, first_name, last_name, role, active, 
-       LEFT(password, 7) as pass_start, LENGTH(password) as pass_length 
-FROM users 
-WHERE email = 'admin@quicktable.com';
